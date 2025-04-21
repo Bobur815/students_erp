fcc10aa7c7eb703c862fb08ee24d69d599d3aaca
Abdurahmon <ibrokhimoffabdurakhmon3@gmail.com>
2025-04-21 18:40:16 +0500

    delete created

--- controllers/index.js
+++ controllers/index.js
@@ -9,9 +9,16 @@ const POST  = (req,res) => {
 const PUT  = (req,res) => {
     
 }
-const DELETE  = (req,res) => {
-    
-}
+const DELETE = (req, res) => {
+    try {
+        const { id } = req.params;        
+        res.status(200).json({ message: `Item with id ${id} deleted successfully.` });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal Server Error', error: error.message });
+    }
+};
+
 
 export default{
     GET,
