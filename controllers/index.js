76efe52c09ea999fa0d7df7e84b062d78e75d7fe
Omadbek <shustrbola@gmail.com>
2025-04-21 15:28:42 +0500

    user_post buldi

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 import fs from "fs"
+import path from "path"
 
 const GET = (req, res) => {
     res.send("get users")
@@ -6,7 +7,14 @@ const GET = (req, res) => {
 
 const USER_POST = (req, res) => {
     const users = req.body
-
+    let userJson = JSON.parse(fs.readFileSync(path.join(process.cwd(), "/db/users.json"), "utf-8"))
+    let newuser = {
+        id: userJson.length ? userJson[userJson.length - 1].id + 1 : 1,
+        createdAt: new Date(),
+        ...users
+    }
+    userJson.push(newuser)
+    fs.writeFileSync(path.join(process.cwd(), "/db/users.json"), JSON.stringify(userJson, null, 2))
 }
 const REGISTER_POST = (req, res) => {
     res.send("register")
