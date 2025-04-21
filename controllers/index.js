22de83d987432453cd0c653445a113271bd21a4b
Boburmirzo Ergashev <ergashevboburmirzo815@gmail.com>
2025-04-21 18:29:36 +0500

    Post yozildi

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,28 @@
 import fs from "fs"
+import path from "path"
 
 const GET  = (req,res) => {
     
 }
 const POST  = (req,res) => {
-    
+    let newUser = req.body;
+    let users = JSON.parse(fs.readFileSync(path.join(process.cwd(),"/db/users.json"),"utf-8"))
+    let obj = {
+        id: users.length ? users.at(-1).id + 1 : 1,
+        ...newUser
+    }
+
+    users.push(obj)
+    fs.writeFileSync(path.join(process.cwd(),"/db/users.json"),JSON.stringify(users,null,4))
+    res.status(201).json({message: "New user created!"})
+
 }
-const PUT  = (req,res) => {
-    
+const PUT = (req,res) => {
+
 }
-const DELETE  = (req,res) => {
-    
+
+const DELETE = (req,res) => {
+
 }
 
 export default{
